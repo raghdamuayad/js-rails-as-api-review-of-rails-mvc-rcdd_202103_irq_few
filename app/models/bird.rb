0b1ad9634e0e4777e6f646a22686aedc9a7044d2
Raghda  Muayad <therose000777@gmail.com>
2021-05-17 10:30:40 +0000

    Done.

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,1 +1,10 @@
 # Add code from Readme
+
+
+  create_table "birds", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false  end
+
+end
